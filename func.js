ccb8eb02176aef12e4e5ede19e48b6c7eaf4f426
angaa123 <angaa123@gmail.com>
2024-06-17 10:31:04 +0800

    func

---
+++ func.js
@@ -0,0 +1,37 @@
+// console.log(hoyou_toonii_niilber(10, 20));
+// function hoyou_toonii_niilber(a, b) {
+// 	console.log(a, b, "hoyor too nii niilber", a + b);
+// }
+
+let a = 10;
+let b = 20;
+function hevel() {
+	console.log(a, b, "hevel", a + b);
+}
+function hevel1(too1, too2) {
+	console.log(too1, too2, "hevel", too1 + too2);
+}
+function hevel2(x, y) {
+	return x + y;
+}
+hevel();
+hevel1(10, 20);
+console.log(hevel2(10, 20) + 10);
+//armetiin 4n uildel
+console.log("armetiin 4 n uildel");
+function nemeh(x, y) {
+	return x + y;
+}
+function hasah(x, y) {
+	return x - y;
+}
+function urjver(x, y) {
+	return x * y;
+}
+function huvaah(x, y) {
+	return x / y;
+}
+console.log(a, "+", b, "=", nemeh(a, b));
+console.log(a, "-", b, "=", hasah(a, b));
+console.log(a, "*", b, "=", urjver(a, b));
+console.log(a, "/", b, "=", huvaah(a, b));
